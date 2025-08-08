87b3b458a0a31244b0ce2d5ac0fa3fd261b4ccc2
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-08-07 21:38:36 -0400

    aula 1: criação dos projetos e configuração inicial - ok - Rodrigo

---
+++ src/main/java/br/com/rodrigobs/dio/storefront/controller/ProductController.java
@@ -0,0 +1,14 @@
+package br.com.rodrigobs.dio.storefront.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("products")
+@AllArgsConstructor
+public class ProductController {
+
+}
---
+++ src/test/java/br/com/rodrigobs/dio/storefront/StorefrontApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigobs.dio.storefront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorefrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
